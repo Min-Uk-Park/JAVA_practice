10a413362bf8e027bc0f3c4aa9068370656fab33
ParkMinUk <parkmxxuk@gmail.com>
2023-08-09 16:27:15 +0900

    Scanner

--- J0809/src/j0809/J0809.java
+++ J0809/src/j0809/J0809.java
@@ -1,9 +1,17 @@
 package j0809;
 
-
+import java.util.Scanner;
 
 public class J0809 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        System.out.print("숫자를 입력하세요. ");
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        System.out.println("입력한 숫자는 " + a + "입니다. ");
+
     }
 }
